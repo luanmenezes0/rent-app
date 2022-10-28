159e691cc88f12d1b5ae006dafc3118128a1a6fa
Luan Menezes <luanmenezes99@gmail.com>
2022-10-28 19:00:54 -0300

    dashboard

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,33 +1,47 @@
 import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { useEffect } from "react";
 import Header from "~/components/Header";
-import { getInventory } from "~/models/delivery.server";
+import type { Rentable } from "~/models/inventory.server.";
 import { getRentables } from "~/models/inventory.server.";
 import { requireUserId } from "~/session.server";
 
 export async function loader({ request }: LoaderArgs) {
   await requireUserId(request);
 
-  const { _sum } = await getInventory(1);
-
   const rentables = await getRentables();
 
-  return json({ inventory: _sum.count, rentables });
+  return json({ rentables });
+}
+
+function Card({ rentable }: { rentable: Rentable }) {
+  const fetcher = useFetcher<{ inventory: number | null }>();
+
+  useEffect(() => {
+    if (fetcher.type === "init") {
+      fetcher.load(`/rentablesinventory/?id=${rentable.id}`);
+    }
+  }, [fetcher, rentable.id]);
+
+  return (
+    <article className="flex flex-col gap-4 bg-red-500">
+      <div>NOME: {rentable.name}</div>
+      <div>TOTAL: {rentable.count}</div>
+      <div>ALUGADO: {fetcher.data?.inventory}</div>
+    </article>
+  );
 }
 
 export default function Index() {
-  const { inventory, rentables } = useLoaderData<typeof loader>();
+  const { rentables } = useLoaderData<typeof loader>();
+
   return (
     <>
       <Header />
       <main className="flex h-full flex-col gap-4 p-8">
         {rentables.map((item) => (
-          <p key={item.id}>
-            <div>{item.name}</div>
-            <div>ALUGADO: {inventory}</div>
-            <div>TOTAL: {item.count}</div>
-          </p>
+          <Card key={item.id} rentable={item} />
         ))}
       </main>
     </>
