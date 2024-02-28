685092d2456726246f6e8ecea1ddeb64af06e83e
Luan Menezes <lmenezes@fullstacklabs.co>
2024-02-28 17:16:23 -0300

    fix

--- remix.config.js
+++ remix.config.js
@@ -6,7 +6,7 @@ const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  serverModuleFormat: 'cjs',
+  serverModuleFormat: "cjs",
   routes(defineRoutes) {
     // uses the v1 convention, works in v1.15+ and v2
     return createRoutesFromFolders(defineRoutes);
